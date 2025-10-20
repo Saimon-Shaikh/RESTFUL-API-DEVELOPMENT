65c0bf137b372b8391eea70a76809dbbfd66b1aa
Saimon Shaikh <saimonshaikh1234@gmail.com>
2025-10-20 13:18:54 +0530

    Primary Flask Program

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Welcome To My Project")
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1> Hello, Saimon! You have Successfully executed the Flask Program. </h1>"
+
+if __name__ == '__main__':
+    app.run (debug=True)
